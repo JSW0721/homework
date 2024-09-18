4a9a81a6a662399d216ac06d2ebe937faa9b5812
JSW0721 <stmjdi04@gmail.com>
2024-09-18 17:29:31 +0900

    Update Main.java

--- homework/src/Main.java
+++ homework/src/Main.java
@@ -1,13 +1,11 @@
 import java.math.BigDecimal;
 
-public class Main {
-    public static void main(String[] args) {
-        Product beauty = new Beauty("beauty", new BigDecimal(3000), 2);
-        Product grocery = new Grocery("grocery", new BigDecimal(4000), 3);
-        Product largeAppliance = new LargeAppliance("largeAppliance", new BigDecimal(1000), 5);
+public static void main(String[] args) {
+	Product beauty = new Beauty("beauty", 30000, 2);
+	Product grocery = new Grocery("grocery", 20000, 3);
+	Product largeAppliance = new LargeAppliance("largeAppliance", 50000, 5);
 
-        Cart cart = new Cart(new Product[] {beauty, grocery, largeAppliance});
-        int totalDeliveryCharge = cart.calculateDeliveryCharge();
-        System.out.println(totalDeliveryCharge);
-    }
+	Cart cart = new Cart(new Product[] {beauty, grocery, largeAppliance});
+	int totalDeliveryCharge = cart.calculateDeliveryCharge();
+	System.out.println(totalDeliveryCharge);    // 결과: 9000
 }
